e03f860370f2ca7f5af58151864b61d94e0f67c2
Dave Bethke <david.bethke@gmail.com>
2012-04-02 22:55:12 -0500

    more stuff

--- RationalNumberApp.cpp
+++ RationalNumberApp.cpp
@@ -6,25 +6,39 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <iomanip>
 
 using namespace std;
 bool isIntChar(char );
-int getMyNum(istream& is,char delim)  // deliminator is '/' or '\n' 
+bool isWhiteSpaceChar(char);
+int getMyNum(istream& is,char delim,bool allowWS=false)  // deliminator is '/' or '\n' 
 {
 	stringstream ss;
+	bool foundFirst=false;
 	char c='0';
 	int myNum=-99;
-	
-	while(is.get(c) && !is.fail() &&  c!=delim)
+	is>>ws;  // built in skip leading whitespace
+	while(is.get(c) &&  c!=delim)
 		{
-			
+			if((!foundFirst)	&& c=='-') //check for a leading negative
+			{
+				ss<<c; //first char can b negative (-)
+				foundFirst=true;
+				is.get(c); //get next
+			}
 			if(!isIntChar(c))
 			{
-				is.ignore(256,delim);
-				is.setstate(ios::failbit);
+				if( allowWS && isWhiteSpaceChar(c))
+					; // skip ignore is WS is allowed w/in the number, 12 3/456 => 123/456
+				else
+				{
+					is.ignore(256,delim); // fails for >256 chars, okay by me
+					is.setstate(ios::failbit);
+				}
 				
 			}
 			else
+				(!foundFirst?foundFirst=true:foundFirst=true);
 				ss<<c;
 		}
 		ss>> myNum;
@@ -38,13 +52,24 @@ int getMyDenom(ifstream& is) // delim is '\n'
 }
 bool isIntChar(char c)
 {
-	return (c=='0'||c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9');
+	//return (c=='0'||c=='1'||c=='2'||c=='3'||c=='4'||c=='5'||c=='6'||c=='7'||c=='8'||c=='9');
+	return (c>='0' && c<='9');
+}
+bool isWhiteSpaceChar(char c)
+{
+	return (c==' ' || c=='\t' || c=='\r');
+}
+void setSkipWs(istream& is)
+{
+	//is.setf(ios::skipws);
+	is>>ws; // built in function
 }
 int _tmain(int argc, _TCHAR* argv[])
 {
 	//ifstream myFile("\goodFile.txt");
 	int num=0, denom=1;
 	char c='\n';
+	
 	while(c!='n' || (!cin))
 	{
 		char divideBy='/';
@@ -52,7 +77,8 @@ int _tmain(int argc, _TCHAR* argv[])
 		cin.ignore(256,'\n');
 		
 		cout << "Enter rational number:";
-		num=getMyNum(cin,divideBy);
+
+		num=getMyNum(cin,divideBy,true);
 		denom=getMyNum(cin,newline);
 		cout <<"Num:"<<num;
 		cout <<"Denom:"<<denom;
